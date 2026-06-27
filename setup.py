2bb812385d053ff61fcacd2da2b3b7f2fca979dc
ShaddAhmed14 <sharjeelshaikh509@gmail.com>
2026-06-27 15:10:29 +0200

    allow-single-video-processing

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name="envisionhgdetector",
-    version="3.0.4",
+    version="3.0.5",
     author="Wim Pouw, Sharjeel Ahmed Shaikh, James Trujillo, Bosco Yung,  Antonio Rueda-Toicen, Gerard de Melo, Babajide Owoyele",
     author_email="w.pouw@tilburguniversity.edu",
     description="Hand gesture detection using MediaPipe and CNN, kinematic analysis, and visualization.",
